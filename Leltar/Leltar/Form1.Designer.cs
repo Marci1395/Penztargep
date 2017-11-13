d55c387ed544df7994041ff0b3a33adac70e81ae
istvan92l <32639266+istvan92l@users.noreply.github.com>
2017-11-13 14:48:05 +0100

    Leltár modul alapjai
    Termékek keresése vonalkód alapján, darabszám megadásával hozzáadja az adatbázishoz, meglévő termék esetén csak darabszámot növel.

---
+++ Leltar/Leltar/Form1.Designer.cs
@@ -0,0 +1,249 @@
+﻿namespace Leltar
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dgv2 = new System.Windows.Forms.DataGridView();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(331, 279);
+            this.textBox6.Multiline = true;
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(317, 179);
+            this.textBox6.TabIndex = 117;
+            this.textBox6.TabStop = false;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(24, 404);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(52, 13);
+            this.label5.TabIndex = 116;
+            this.label5.Text = "Vonalkód";
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.BackgroundColor = System.Drawing.SystemColors.ControlLightLight;
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(11, 28);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(10, 10);
+            this.dataGridView2.TabIndex = 113;
+            this.dataGridView2.Visible = false;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ControlLightLight;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(27, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(621, 223);
+            this.dataGridView1.TabIndex = 112;
+            this.dataGridView1.TabStop = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(23, 445);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(60, 13);
+            this.label4.TabIndex = 111;
+            this.label4.Text = "Darabszám";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(23, 361);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 13);
+            this.label3.TabIndex = 110;
+            this.label3.Text = "Netto ár";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(24, 320);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 13);
+            this.label2.TabIndex = 109;
+            this.label2.Text = "Megnevezés";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 285);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(18, 13);
+            this.label1.TabIndex = 108;
+            this.label1.Text = "ID";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(116, 279);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(189, 20);
+            this.textBox5.TabIndex = 115;
+            this.textBox5.TabStop = false;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(116, 438);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 105;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(232, 435);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 107;
+            this.button1.TabStop = false;
+            this.button1.Text = "Mentés";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dgv2
+            // 
+            this.dgv2.BackgroundColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.dgv2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv2.Location = new System.Drawing.Point(11, 12);
+            this.dgv2.Name = "dgv2";
+            this.dgv2.Size = new System.Drawing.Size(10, 10);
+            this.dgv2.TabIndex = 106;
+            this.dgv2.Visible = false;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(116, 397);
+            this.textBox4.MaxLength = 13;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 104;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(232, 394);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 103;
+            this.button3.TabStop = false;
+            this.button3.Text = "Keres";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(116, 354);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(189, 20);
+            this.textBox3.TabIndex = 102;
+            this.textBox3.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(116, 313);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(189, 20);
+            this.textBox1.TabIndex = 114;
+            this.textBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1062, 599);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dgv2);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dgv2;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox1;
+
+    }
+}
+
---
+++ Leltar/Leltar/Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+using System.IO;
+
+namespace Leltar
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int current_rec;
+        int number_of_rec; DataTable dt;
+        private void fill()
+        {
+            textBox1.Text = dt.Rows[current_rec]["Megnevezes"].ToString();
+            textBox3.Text = dt.Rows[current_rec]["Nettoar"].ToString();
+            textBox5.Text = dt.Rows[current_rec]["ID"].ToString();
+        }
+        void reset()
+        {
+            textBox5.Text = "";
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+            textBox4.Text = "";
+            this.ActiveControl = textBox4;
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string connstring = "Provider=Microsoft.ACE.OLEDB.12.0; data source = " + @"C:\VS\Arukeszlet.accdb";
+                OleDbConnection conn = new OleDbConnection(connstring);
+                conn.Open();
+                string keres = textBox4.Text.ToString();
+                string commandString = " Select ID FROM Vonalkodok Where Vonalkod = '" + keres + "'";
+                OleDbDataAdapter dataadapter = new OleDbDataAdapter(commandString, conn);
+                DataSet ds = new DataSet();
+                dataadapter.Fill(ds, "Vonalkodok");
+
+                string id = ds.Tables[0].Rows[0][0].ToString();
+                int i = int.Parse(id);
+
+                commandString = " Select * FROM Arukeszlet Where ID =" + id + "";
+                dataadapter = new OleDbDataAdapter(commandString, conn);
+                ds = new DataSet();
+                dataadapter.Fill(ds, "Arukeszlet");
+                dgv2.DataSource = ds;
+                dgv2.DataMember = "Arukeszlet";
+                conn.Close();
+
+                dt = ds.Tables["Arukeszlet"];
+                current_rec = 0;
+                number_of_rec = dt.Rows.Count;
+                fill();
+                textBox4.Text = "";
+                textBox2.Text = "";
+
+            }
+            catch { }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string id = textBox5.Text;
+            string mn = textBox1.Text;
+            string db = textBox2.Text;
+            string na = textBox3.Text;
+
+
+
+            string connstring = "Provider=Microsoft.ACE.OLEDB.12.0; data source = " + @"C:\VS\Arukeszlet.accdb";
+            OleDbConnection conn = new OleDbConnection(connstring);
+            conn.Open();
+
+            try
+            {
+                string commandString = "INSERT INTO Leltar (ID, Megnevezes, Darabszam, Nettoar) VALUES ('" + id + "', '" + mn + "', '" + db + "', '" + na + "')";
+                OleDbCommand cmd = new OleDbCommand(commandString, conn);
+                cmd.ExecuteNonQuery();
+                textBox6.Text += "" + mn + "   " + db + "db";
+                textBox6.Text += "\r\n";
+            }
+            catch
+            {
+                connstring = " Select Darabszam FROM Leltar Where ID = " + id + "";
+                OleDbDataAdapter dataadapter1 = new OleDbDataAdapter(connstring, conn);
+                DataSet ds1 = new DataSet();
+                dataadapter1.Fill(ds1, "Leltar");
+                ds1 = new DataSet();
+                dataadapter1.Fill(ds1, "Leltar");
+                dataGridView2.DataSource = ds1;
+                dataGridView2.DataMember = "Leltar";
+                string db1 = ds1.Tables[0].Rows[0][0].ToString();
+
+                int a = int.Parse(db1);
+                int db2 = int.Parse(db);
+                int sum = a + db2;
+                string sum1 = sum.ToString();
+                string commandString = "UPDATE Leltar SET Darabszam = " + sum1 + " Where ID = " + id + "";
+                OleDbCommand cmd = new OleDbCommand(commandString, conn);
+                cmd.ExecuteNonQuery();
+                textBox6.Text += "" + mn + "   " + a + "+" + db2 + " -> " + sum1 + "db";
+                textBox6.Text += "\r\n";
+
+            }
+
+            connstring = " Select * FROM Leltar";
+            OleDbDataAdapter dataadapter2 = new OleDbDataAdapter(connstring, conn);
+            DataSet ds2 = new DataSet();
+            dataadapter2.Fill(ds2, "Leltar");
+            ds2 = new DataSet();
+            dataadapter2.Fill(ds2, "Leltar");
+            dataGridView1.DataSource = ds2;
+            dataGridView1.DataMember = "Leltar";
+            conn.Close();
+            reset();
+        }
+    }
+}
