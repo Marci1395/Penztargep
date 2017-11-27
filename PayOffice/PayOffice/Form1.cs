e4094f17aa37da42044cff9c65259758cf2072fc
istvan92l <l.istvan92@gmail.com>
2017-11-27 15:57:45 +0100

    Váz
    Igényeknek megfelelően, átlett alakítva az egész program szerkezete és ebben fogjuk a különbözö modulokat beépíteni.

---
+++ PayOffice/PayOffice/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PayOffice
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string username = textBox1.Text;
+            string password = textBox2.Text;
+            string connstring = "Provider=Microsoft.ACE.OLEDB.12.0; data source = DB.accdb;";
+            OleDbConnection conn = new OleDbConnection(connstring);
+            conn.Open();
+            string commandString = " Select Password FROM Users Where Username = '" + username + "'";
+            OleDbDataAdapter dataadapter = new OleDbDataAdapter(commandString, conn);
+            DataSet ds = new DataSet();
+            dataadapter.Fill(ds, "Users");
+            try
+            {
+                string pw = ds.Tables[0].Rows[0][0].ToString();
+                if (password == pw)
+                {
+                    Main main = new Main();
+                    this.Hide();
+                    main.Show();
+                }
+                else
+                {
+                    MessageBox.Show("Nem megfelelő felhasználónév vagy jelszó!");
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Nincs ilyen felhasználó");
+            }
+            
+            conn.Close();
+
+        }
+    }
+}
---
+++ PayOffice/PayOffice/Main.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace PayOffice
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.újToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.módosításToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.újToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.vásárlókToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.újToolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.szállítókToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.számlákToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pénztárToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.eszközökToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.naplóToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.leltárToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1,
+            this.toolStripMenuItem2,
+            this.vásárlókToolStripMenuItem,
+            this.szállítókToolStripMenuItem,
+            this.számlákToolStripMenuItem,
+            this.pénztárToolStripMenuItem,
+            this.eszközökToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(610, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.újToolStripMenuItem,
+            this.módosításToolStripMenuItem});
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(86, 20);
+            this.toolStripMenuItem1.Text = "Felhasználók";
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.újToolStripMenuItem1});
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(71, 20);
+            this.toolStripMenuItem2.Text = "Termékek";
+            // 
+            // újToolStripMenuItem
+            // 
+            this.újToolStripMenuItem.Name = "újToolStripMenuItem";
+            this.újToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.újToolStripMenuItem.Text = "Új";
+            // 
+            // módosításToolStripMenuItem
+            // 
+            this.módosításToolStripMenuItem.Name = "módosításToolStripMenuItem";
+            this.módosításToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.módosításToolStripMenuItem.Text = "Módosítás";
+            // 
+            // újToolStripMenuItem1
+            // 
+            this.újToolStripMenuItem1.Name = "újToolStripMenuItem1";
+            this.újToolStripMenuItem1.Size = new System.Drawing.Size(152, 22);
+            this.újToolStripMenuItem1.Text = "Új";
+            // 
+            // vásárlókToolStripMenuItem
+            // 
+            this.vásárlókToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.újToolStripMenuItem2});
+            this.vásárlókToolStripMenuItem.Name = "vásárlókToolStripMenuItem";
+            this.vásárlókToolStripMenuItem.Size = new System.Drawing.Size(63, 20);
+            this.vásárlókToolStripMenuItem.Text = "Vásárlók";
+            // 
+            // újToolStripMenuItem2
+            // 
+            this.újToolStripMenuItem2.Name = "újToolStripMenuItem2";
+            this.újToolStripMenuItem2.Size = new System.Drawing.Size(152, 22);
+            this.újToolStripMenuItem2.Text = "Új";
+            // 
+            // szállítókToolStripMenuItem
+            // 
+            this.szállítókToolStripMenuItem.Name = "szállítókToolStripMenuItem";
+            this.szállítókToolStripMenuItem.Size = new System.Drawing.Size(62, 20);
+            this.szállítókToolStripMenuItem.Text = "Szállítók";
+            // 
+            // számlákToolStripMenuItem
+            // 
+            this.számlákToolStripMenuItem.Name = "számlákToolStripMenuItem";
+            this.számlákToolStripMenuItem.Size = new System.Drawing.Size(62, 20);
+            this.számlákToolStripMenuItem.Text = "Számlák";
+            // 
+            // pénztárToolStripMenuItem
+            // 
+            this.pénztárToolStripMenuItem.Name = "pénztárToolStripMenuItem";
+            this.pénztárToolStripMenuItem.Size = new System.Drawing.Size(58, 20);
+            this.pénztárToolStripMenuItem.Text = "Pénztár";
+            // 
+            // eszközökToolStripMenuItem
+            // 
+            this.eszközökToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.naplóToolStripMenuItem,
+            this.leltárToolStripMenuItem});
+            this.eszközökToolStripMenuItem.Name = "eszközökToolStripMenuItem";
+            this.eszközökToolStripMenuItem.Size = new System.Drawing.Size(66, 20);
+            this.eszközökToolStripMenuItem.Text = "Eszközök";
+            // 
+            // naplóToolStripMenuItem
+            // 
+            this.naplóToolStripMenuItem.Name = "naplóToolStripMenuItem";
+            this.naplóToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.naplóToolStripMenuItem.Text = "Napló";
+            // 
+            // leltárToolStripMenuItem
+            // 
+            this.leltárToolStripMenuItem.Name = "leltárToolStripMenuItem";
+            this.leltárToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.leltárToolStripMenuItem.Text = "Leltár";
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(610, 326);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Main";
+            this.Text = "Főoldal";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem újToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem módosításToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem újToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem vásárlókToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem újToolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem szállítókToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem számlákToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem pénztárToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem eszközökToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem naplóToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem leltárToolStripMenuItem;
+    }
+}
